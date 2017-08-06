833f37e6a1fc18a7fc88b546326ce2831c9b91b8
Jack <me@jarrekk.com>
2017-08-06 16:57:38 +0800

    edit celery

--- application/taskapp/celery.py
+++ application/taskapp/celery.py
@@ -10,13 +10,14 @@ import os
 from celery import Celery
 from django.conf import settings
 
-# set the default Django settings module for the 'celery' program.
+# set Django settings module for celery program.
 config = '.'.join(['settings', os.environ.get('ENV', 'development')])
 os.environ['DJANGO_SETTINGS_MODULE'] = config
 
 app = Celery(os.environ.get('PROJECT_NAME'))
-
 app.config_from_object('django.conf:settings')
+
+# auto discover tasks on installed apps & project root folder
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS + [os.environ.get('PROJECT_NAME') + '.tasks'])
 
 
